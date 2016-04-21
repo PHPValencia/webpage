98812b393e1d7d9a91106003335fa35a08677d38
Carlos Reig Matut <unstatu@gmail.com>
2016-04-21 14:05:11 +0200

    Added the host-the-event page

---
+++ source/host-the-event.blade.php
@@ -0,0 +1,27 @@
+@extends('_layouts.master')
+
+@section('contents')
+    <p class="eyelet margin_top_0">
+        Queremos dar visibilidad a las empresas que hacen PHP en Valencia
+    </p>
+    <h2 class="centered orange scream">
+        ¿Quieres echar una mano a la comunidad? ¡Aloja el evento de PHPValencia del mes que viene!
+    </h2>
+    <div class="clearfix">
+        <section class="content single">
+            <h2>Cómo hacerlo</h2>
+            <p>Para alojar la próxima reunión de PHP Valencia simplemente tienes que contactes con nosotros por cualquiera de las vías de contacto que están en la parte inferior de la página.</p>
+            <p>Lo único que tienes que tener en cuenta es:</p>
+            <ul>
+                <li>No hay compensación económica por alojar el evento</li>
+                <li>El aforo mínimo debe de ser para 25 personas</li>
+                <li>Es necesario tener proyector y pantalla/pared donde proyectar</li>
+                <li>Es necesario que haya sillas para los asistentes</li>
+                <li>Es preferible que haya nevera</li>
+                <li>Es preferible que el espacio tenga un sistema de grabación</li>
+                <li>Es preferible que el espacio esté en Valencia capital</li>
+            </ul>
+            <p>No es mucho ¿verdad? ¡Anímate! :)</p>
+        </section>
+    </div>
+@endsection
