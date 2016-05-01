59e86f4acdf0262ee4ff7da1f4852c90b2c21cb8
David P <david@davidpena.es>
2016-05-01 17:22:12 +0200

    add favicons

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -4,10 +4,30 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
     <title>PHP Valencia</title>
     <meta name="description" content="phpvalencia.es es un grupo local de desarrolladores web que usan el lenguaje #PHP." />
     <meta name="author" content="Valentin Rusev, Carlos Reig, Stephen O'Flynn, David Peña" />
+
+    <link rel="apple-touch-icon" sizes="57x57" href="/favicons/apple-touch-icon-57x57.png?v=XBBlYrBb4N">
+    <link rel="apple-touch-icon" sizes="60x60" href="/favicons/apple-touch-icon-60x60.png?v=XBBlYrBb4N">
+    <link rel="apple-touch-icon" sizes="72x72" href="/favicons/apple-touch-icon-72x72.png?v=XBBlYrBb4N">
+    <link rel="apple-touch-icon" sizes="76x76" href="/favicons/apple-touch-icon-76x76.png?v=XBBlYrBb4N">
+    <link rel="apple-touch-icon" sizes="114x114" href="/favicons/apple-touch-icon-114x114.png?v=XBBlYrBb4N">
+    <link rel="apple-touch-icon" sizes="120x120" href="/favicons/apple-touch-icon-120x120.png?v=XBBlYrBb4N">
+    <link rel="apple-touch-icon" sizes="144x144" href="/favicons/apple-touch-icon-144x144.png?v=XBBlYrBb4N">
+    <link rel="apple-touch-icon" sizes="152x152" href="/favicons/apple-touch-icon-152x152.png?v=XBBlYrBb4N">
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon-180x180.png?v=XBBlYrBb4N">
+    <link rel="icon" type="image/png" href="/favicons/favicon-32x32.png?v=XBBlYrBb4N" sizes="32x32">
+    <link rel="icon" type="image/png" href="/favicons/android-chrome-192x192.png?v=XBBlYrBb4N" sizes="192x192">
+    <link rel="icon" type="image/png" href="/favicons/favicon-96x96.png?v=XBBlYrBb4N" sizes="96x96">
+    <link rel="icon" type="image/png" href="/favicons/favicon-16x16.png?v=XBBlYrBb4N" sizes="16x16">
+    <link rel="manifest" href="/favicons/manifest.json?v=XBBlYrBb4N">
+    <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg?v=XBBlYrBb4N" color="#5bbad5">
+    <link rel="shortcut icon" href="/favicons/favicon.ico?v=XBBlYrBb4N">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="msapplication-TileImage" content="/favicons/mstile-144x144.png?v=XBBlYrBb4N">
+    <meta name="theme-color" content="#ffffff">
+
     <meta name="viewport" content="width=device-width" />
     <link href='http://fonts.googleapis.com/css?family=Rambla:400,700' rel='stylesheet' type='text/css' />
     <link rel="stylesheet" href="/css/normalize.css" />
