e665937148a974b8c686e381915aa25e3dcae6e0
Valentin Boyanov <val.rusev@gmail.com>
2016-05-02 16:53:07 +0200

    Next meetup: 05-may-2016

--- source/past.blade.php
+++ source/past.blade.php
@@ -1,6 +1,23 @@
 @extends('_layouts.master')
 
 @section('contents')
+    <h3 class="centered past">jueves 7 abril 2016, 19:00h</h3>
+
+    <div class="clearfix">
+        <section class="content single">
+            <h2>Refactoring colaborativo</h2>
+            <p>¿Alguna vez te ha pasado estar trabajando en un código que cada vez se te atraganta más? Ya no es mantenible y cada nuevo cambio te cuesta más tiempo y esfuerzo que el anterior.</p>
+            <p>Creo que todos hemos experimentado esa sensación... Y no mola nada.</p>
+            <p>Afortunadamente, como casi todo en la vida, ese problema tiene una solución: <b>refactorizar tu código</b>.</p>
+            <p>Pues bien, en esta ocasión, la reunión de PHP Valencia no va a consistir en una charla como hacemos habitualmente. En lugar de zeso, <b>haremos ¡una sesión de refactoring colaborativa!</b></p>
+            <p>Valentin Rusev (<a href="https://twitter.com/kolbyfloyd">@kolbyfloyd</a>), un conocido miembro de la comunidad, tiene un problema con el código de un componente de su proyecto: <a href="http://festop.com">Festop</a>. El objetivo de la reunión es que analicemos su problema y refactoricemos su código entre todos.</p>
+            <p>Si no sabes muy bien en qué consiste un proceso de <b>refactor</b>, te diré que <b>hablaremos de testing, patrones y buenas prácticas</b>. Así que, ¡no pierdas esta oportunidad de aprender!</p>
+            <p>Por otra parte,<b>&nbsp;la reunión de este mes tendrá lugar en las oficinas de <a href="http://onestic.com/">Onestic</a>&nbsp;</b>(C/ Roger de Lauria 19, 4º piso, Despacho B), que se ha ofrecido amablemente a acogernos.</p>
+            <p>Y recuerda que, después de la reunión, algunos nos quedaremos a cenar en algún bar de la zona. ¡Apúntate a la cena! que es una ocasión perfecta para conocernos mejor :)</p>
+            <p>¡Nos vemos el jueves 7 de abril a las 19:00 en las oficinas de Onestic!</p>
+        </section>
+    </div>
+
     <h3 class="centered past">jueves 3 marzo 2016, 19:00h</h3>
 
     <div class="clearfix">
