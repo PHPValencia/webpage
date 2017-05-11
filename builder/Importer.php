2480af3122488804ae43c8d9a89a2945ccaa3cd4
Carlos Reig Matut <unstatu@gmail.com>
2017-05-11 19:26:58 +0200

    ADded Value Objects for events and venue

---
+++ builder/Importer.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: statu
+ * Date: 5/8/17
+ * Time: 8:55 PM
+ */
+
+namespace Builder;
+
+
+class ImporterTest {
+
+}
---
+++ builder/VO/Event.php
@@ -0,0 +1,12 @@
+<?php
+namespace Builder\VO;
+
+class Event {
+
+	public $name;
+	public $description;
+	public $timestamp;
+
+	/** @var  Venue */
+	public $venue;
+}
