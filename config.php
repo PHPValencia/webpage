4ee2f818ad9b4ea65ad5a4b1dfa823e5ac7b2bd8
Valentin Boyanov <valentin@onestic.com>
2019-02-21 08:37:36 +0100

    Remove date formatting from Jigsaw.

--- config.php
+++ config.php
@@ -7,9 +7,6 @@ return [
         'description' => 'Grupo local de desarrolladores web que usan el lenguaje #PHP. Y, como siempre, después de cada charla, algunos van a cenar por la zona, ¡todos sois bienvenidos!'
     ],
     'collections' => ['events'],
-    'formattedDate' => function ($page, $date) {
-        return (new \DateTime($date))->format('d/m/Y');
-    },
     'menu' => [
         [
             'title' => '¿Qué es?',
